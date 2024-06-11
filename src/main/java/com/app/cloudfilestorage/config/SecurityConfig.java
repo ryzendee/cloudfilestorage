f852e8e51ac6c7150a616d33e57c49281a8b5005
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-06-11 09:22:02 +0400

    feat: Added security configuration

---
+++ src/main/java/com/app/cloudfilestorage/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.app.cloudfilestorage.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .formLogin(formConfigurer -> formConfigurer
+                        .loginPage("/login")
+                        .permitAll()
+                ).logout(logoutConfigurer -> logoutConfigurer
+                        .logoutUrl("/logout")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                ).authorizeHttpRequests(requestMatcher -> requestMatcher
+                        .requestMatchers(
+                                "/signup",
+                                "/login",
+                                "/logout",
+                                "/access-denied").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .authenticationProvider(daoAuthenticationProvider())
+                .exceptionHandling(handlingConfigurer ->
+                        handlingConfigurer.accessDeniedPage("/access-denied")
+                );
+
+        return httpSecurity.build();
+    }
+}
