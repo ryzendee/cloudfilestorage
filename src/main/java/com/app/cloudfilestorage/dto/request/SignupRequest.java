a79cbf2587d7be7fe86eb33219220e14b6a74d53
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-06-11 12:06:26 +0400

    refactor: Fixed grammar mistake in passwordConfirmation field

--- src/main/java/com/app/cloudfilestorage/dto/request/SignupRequest.java
+++ src/main/java/com/app/cloudfilestorage/dto/request/SignupRequest.java
@@ -1,27 +1,27 @@
-package com.app.cloudfilestorage.dto.request;
+    package com.app.cloudfilestorage.dto.request;
 
-import com.app.cloudfilestorage.validation.PasswordMatcher;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+    import com.app.cloudfilestorage.validation.PasswordMatcher;
+    import jakarta.validation.constraints.NotBlank;
+    import jakarta.validation.constraints.Size;
+    import lombok.AllArgsConstructor;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+    import lombok.Setter;
 
-@PasswordMatcher
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class SignupRequest {
+    @PasswordMatcher
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class SignupRequest {
 
-    @Size(min = 5, message = "Password must be at least 5 symbols length")
-    @NotBlank(message = "Username is required")
-    private String username;
+        @Size(min = 5, message = "Username must be at least 5 symbols length")
+        @NotBlank(message = "Username is required")
+        private String username;
 
-    @Size(min = 5, message = "Password must be at least 5 symbols length")
-    @NotBlank(message = "Password is required")
-    private String password;
+        @Size(min = 5, message = "Password must be at least 5 symbols length")
+        @NotBlank(message = "Password is required")
+        private String password;
 
-    private String passwordConfimation;
-}
+        private String passwordConfirmation;
+    }
