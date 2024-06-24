fcae1e43233edce0aaa984fd2d8216e92b1abeea
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-06-24 14:29:30 +0400

    feat: Added method to remove user root folder from path

--- src/main/java/com/app/cloudfilestorage/utils/PathGeneratorUtil.java
+++ src/main/java/com/app/cloudfilestorage/utils/PathGeneratorUtil.java
@@ -5,10 +5,19 @@ public final class PathGeneratorUtil {
     private static final String TEMPLATE = "user-%d-files/";
 
     public static String formatPath(Long userId, String path) {
-        if (path.equals(DEFAULT_PATH)) {
+        if (path.equals(DEFAULT_PATH) || path.isEmpty()) {
             return TEMPLATE.formatted(userId);
         }
 
+        if (path.startsWith(TEMPLATE.formatted(userId))) {
+            return path;
+        }
+
         return TEMPLATE.formatted(userId) + path;
+    }
+
+    public static String removeTemplateFromPath(Long userId, String path) {
+        String pathToRemove = TEMPLATE.formatted(userId);
+        return path.replace(pathToRemove, "");
     }
 }
