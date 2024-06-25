d7bea6a4ae0944cab1cb5bbc80dd1a4382c82e54
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-06-25 14:42:33 +0400

    refactor: Replaced @Data annotation and added validation for fields

--- src/main/java/com/app/cloudfilestorage/dto/request/FolderDeleteRequest.java
+++ src/main/java/com/app/cloudfilestorage/dto/request/FolderDeleteRequest.java
@@ -1,8 +1,23 @@
 package com.app.cloudfilestorage.dto.request;
 
-import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class FolderDeleteRequest {
+    @NotNull(message = "User is not authorized")
+    private Long ownerId;
+    @NotBlank(message = "Folder path must not be blank or null")
     private String folderPath;
+
+    public FolderDeleteRequest(Long ownerId) {
+        this.ownerId = ownerId;
+    }
 }
