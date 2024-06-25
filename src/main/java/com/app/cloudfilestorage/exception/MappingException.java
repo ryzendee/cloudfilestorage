4a080469861f4641df38a9a73ae505a95a390815
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-06-25 12:43:52 +0400

    fix: Added mapping exception

---
+++ src/main/java/com/app/cloudfilestorage/exception/MappingException.java
@@ -0,0 +1,8 @@
+package com.app.cloudfilestorage.exception;
+
+public class MappingException extends RuntimeException {
+
+    public MappingException(Throwable cause) {
+        super(cause);
+    }
+}
