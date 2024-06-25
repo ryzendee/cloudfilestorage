9f89b582dfe3ade1df0c2efab12405f49734b494
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-06-25 11:57:44 +0400

    fix: Added formatPathToFolder method in PathGeneratorUtil

--- src/main/java/com/app/cloudfilestorage/utils/PathGeneratorUtil.java
+++ src/main/java/com/app/cloudfilestorage/utils/PathGeneratorUtil.java
@@ -4,7 +4,7 @@ public final class PathGeneratorUtil {
     private static final String DEFAULT_PATH = "/";
     private static final String TEMPLATE = "user-%d-files/";
 
-    public static String formatPath(Long userId, String path) {
+    public static String formatPathForFolder(Long userId, String path) {
         if (path.equals(DEFAULT_PATH) || path.isEmpty()) {
             return TEMPLATE.formatted(userId);
         }
@@ -14,6 +14,18 @@ public final class PathGeneratorUtil {
         }
 
         return TEMPLATE.formatted(userId) + path;
+    }
+    
+    public static String formatPathForFolder(Long userId, String currentFolderPath, String path) {
+        if (!path.endsWith("/")) {
+            path += "/";
+        }
+
+        if (currentFolderPath.equals("/")) {
+            return TEMPLATE.formatted(userId) + path;
+        }
+
+        return TEMPLATE.formatted(userId) + currentFolderPath + path;
     }
 
     public static String removeTemplateFromPath(Long userId, String path) {
