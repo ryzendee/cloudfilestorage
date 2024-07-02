0d646bf348ae848ada68e92042ea18887014dcee
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-07-02 17:46:25 +0500

    feat: Added dto requests for file

---
+++ src/main/java/com/app/cloudfilestorage/dto/request/FileDeleteRequest.java
@@ -0,0 +1,17 @@
+package com.app.cloudfilestorage.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileDeleteRequest {
+
+    @NotBlank(message = "File path must not be blank")
+    private String path;
+}
---
+++ src/main/java/com/app/cloudfilestorage/dto/request/FileDownloadRequest.java
@@ -0,0 +1,19 @@
+package com.app.cloudfilestorage.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileDownloadRequest {
+
+    @NotBlank(message = "File name must not be blank")
+    private String name;
+    @NotBlank(message = "File path must not be blank")
+    private String path;
+}
