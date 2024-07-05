8b4a384e2421774ee0e74b133efd5290b4c91620
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-07-05 19:05:26 +0500

    feat: Added ex handling for custom exceptions

--- src/main/java/com/app/cloudfilestorage/controller/ControllerExceptionHandler.java
+++ src/main/java/com/app/cloudfilestorage/controller/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.app.cloudfilestorage.controller;
 
+import com.app.cloudfilestorage.exception.FileServiceException;
+import com.app.cloudfilestorage.exception.FolderServiceException;
 import com.app.cloudfilestorage.exception.SignupException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,11 +13,28 @@ import org.springframework.web.servlet.view.RedirectView;
 @Slf4j
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(SignupException.class)
-    public RedirectView handleSignUpException(SignupException ex,
-                                              RedirectAttributes redirectAttributes) {
+    private static final String FLASH_ATR_EXCEPTION_MESSAGE = "exceptionMessage";
+    private static final String HOME_PAGE_URI = "/";
 
-        redirectAttributes.addFlashAttribute("exceptionMessage", ex.getMessage());
+    @ExceptionHandler(SignupException.class)
+    public RedirectView handleSignUpEx(SignupException ex,
+                                       RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute(FLASH_ATR_EXCEPTION_MESSAGE, ex.getMessage());
         return new RedirectView("/signup");
+    }
+
+    @ExceptionHandler(FolderServiceException.class)
+    public RedirectView handleFolderServiceEx(FolderServiceException ex,
+                                              RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute(FLASH_ATR_EXCEPTION_MESSAGE, ex.getMessage());
+        return new RedirectView(HOME_PAGE_URI);
+    }
+
+    @ExceptionHandler(FileServiceException.class)
+    public RedirectView handleFileServiceEx(FileServiceException ex,
+                                            RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute(FLASH_ATR_EXCEPTION_MESSAGE, ex.getMessage());
+        return new RedirectView(HOME_PAGE_URI);
     }
 }
