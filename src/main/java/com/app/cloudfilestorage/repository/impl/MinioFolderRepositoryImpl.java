579fcaef2ab511a5365cffd2c667eec014a88373
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-07-16 13:47:06 +0500

    refactor: Added lastModified init to mapToStatoObjectMethod

--- src/main/java/com/app/cloudfilestorage/repository/impl/MinioFolderRepositoryImpl.java
+++ src/main/java/com/app/cloudfilestorage/repository/impl/MinioFolderRepositoryImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Repository;
 import java.io.*;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -183,7 +184,7 @@ public class MinioFolderRepositoryImpl implements MinioFolderRepository {
                 minioSaveDataDto.objectName(),
                 minioSaveDataDto.inputStream(),
                 minioSaveDataDto.objectSize(),
-                null
+                ZonedDateTime.now()
         );
     }
 
