9d7ee6b1c0d7b4ac923b737427cafeffd731d40d
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-07-26 13:31:52 +0500

    fix: Added host and port

--- src/main/java/com/app/cloudfilestorage/config/RedisConfiguration.java
+++ src/main/java/com/app/cloudfilestorage/config/RedisConfiguration.java
@@ -1,5 +1,6 @@
 package com.app.cloudfilestorage.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -9,8 +10,13 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRedisHttpSession
 public class RedisConfiguration {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+
     @Bean
     public LettuceConnectionFactory lettuceConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(host, port);
     }
 }
